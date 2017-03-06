9037c9a7b2b8c07e6e59b8bd3720641f9d92e0df
Vitor Vilas Boas <vvilas@br.ibm.com>
2017-03-06 14:09:20 -0300

    Authentication Complete

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -6,6 +6,12 @@ var ObjectID = require('mongodb').ObjectID;
 
 var router = function (nav) {
 
+	bookRouter.use(function (req, res, next) {
+		if (!req.user) {
+			res.redirect('/');
+		}
+		next();
+	});
 	bookRouter.route('/')
 		.get(function (req, res) {
 			var url = 'mongodb://localhost:27017/libraryApp';
@@ -28,7 +34,9 @@ var router = function (nav) {
 			var url = 'mongodb://localhost:27017/libraryApp';
 			mongodb.connect(url, function (err, db) {
 				var collection = db.collection('books');
-				collection.findOne({_id: id}, function (err, results) {
+				collection.findOne({
+					_id: id
+				}, function (err, results) {
 					res.render('booksView', {
 						title: 'Books',
 						nav: nav,
